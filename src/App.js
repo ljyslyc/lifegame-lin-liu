17eb1f4c7cd884b241c0dea609cbfe86135df0b6
ljyslyc <37475715+ljyslyc@users.noreply.github.com>
2019-05-05 14:30:48 +0800

    添加csspanel

--- src/App.js
+++ src/App.js
@@ -5,12 +5,15 @@ import LgEntrance from "./LgEntrance";
 
 
 function App() {
-  return (
-    <div className="App">
-      <h1 style={{textAlign:'center'}}>生命游戏</h1>
-      <LgEntrance/>
-    </div>
-  );
+
+
+
+        return (
+            <div className="App">
+                <h1 style={{textAlign: 'center'}}>Conway's Game of Life</h1>
+                <LgEntrance/>
+            </div>
+        );
 }
 
 export default App;
--- src/LgEvolutionPanel.js
+++ src/LgEvolutionPanel.js
@@ -1,4 +1,5 @@
 import React,{Component} from 'react'
+import './LgEvolutionPanel.css'
 
 class LgEvolutionPanel extends Component{
     render() {
